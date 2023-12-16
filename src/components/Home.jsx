f5540d41e2bfdb75e9add71e8e3a8bb5e58bdeff
gokce <gokcetascii@gmail.com>
2023-12-17 00:12:54 +0300

    slider page added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Slider from './Slider';
 
 function Home() {
   return (
     <div>
-      
+      <Slider/>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
