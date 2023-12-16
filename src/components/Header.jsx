d10df41f2278206c174cd64ec6e580530a7fdf08
gokce <gokcetascii@gmail.com>
2023-12-16 23:07:11 +0300

    header page created

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>EXOMAC</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
