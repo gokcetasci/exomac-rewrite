1c1854ea79974c78c29f022a598ef17521d576cb
gokce <gokcetascii@gmail.com>
2023-12-17 02:32:09 +0300

    arrow button added to slider page and style added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import SlickSlider from 'react-slick';
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 
 function Slider() {
   const sliders = [
     {
-      img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-2.jpg"
+      img: 'https://exomac.jamstacktemplates.dev/images/hero-image/hero-2.jpg',
     },
     {
-      img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-1.jpg"
+      img: 'https://exomac.jamstacktemplates.dev/images/hero-image/hero-1.jpg',
     },
     {
-      img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-3.jpg"
-    }
+      img: 'https://exomac.jamstacktemplates.dev/images/hero-image/hero-3.jpg',
+    },
   ];
 
-  const settings = {
+  const settings = { //slider ayarları
     dots: false,
     infinite: true,
     speed: 500,
@@ -26,9 +27,36 @@ function Slider() {
     autoplaySpeed: 3000,
   };
 
+  const sliderRef = useRef(null); //arrow lar için
+  const [isHovered, setIsHovered] = useState(false); //arrowları mouse ile etkinleştirmek için
+
+  const handleMouseEnter = () => { 
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const goToPrevSlide = () => { //önceki resime geçme
+    if (sliderRef.current) {
+      sliderRef.current.slickPrev();
+    }
+  };
+
+  const goToNextSlide = () => { //sonraki resime geçme
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+    }
+  };
+
   return (
-    <div className="slider-container relative">
-      <SlickSlider {...settings} >
+    <div
+      className="slider-container relative"
+      onMouseEnter={handleMouseEnter} 
+      onMouseLeave={handleMouseLeave}
+    >
+      <SlickSlider {...settings} className="relative" ref={sliderRef}>
         {sliders.map((slider, index) => (
           <div key={index} className="slider-item relative">
             <img src={slider.img} alt={`slide ${index + 1}`} />
@@ -36,6 +64,26 @@ function Slider() {
           </div>
         ))}
       </SlickSlider>
+      {isHovered && (
+        <>
+          <div className="absolute left-6 top-80 transform -translate-y-1/2">
+            <button
+              className="rounded-[10px] p-6 bg-black/25 hover:scale-105 transition duration-300 ease-in-out flex justify-center items-center text-white hover:text-primary text-[28px]"
+              onClick={goToPrevSlide}
+            >
+              <IoIosArrowBack/>
+            </button>
+          </div>
+          <div className="absolute right-6 top-80 transform -translate-y-1/2">
+            <button
+              className="rounded-[10px] p-6 bg-black/25 hover:scale-105 transition duration-300 ease-in-out flex justify-center items-center text-white hover:text-primary text-[28px] font-bold"
+              onClick={goToNextSlide}
+            >
+              <IoIosArrowForward />
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
