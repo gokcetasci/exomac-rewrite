6e4a13285f728b566350c72223a15951fa7f8ea1
gokce <gokcetascii@gmail.com>
2023-12-17 00:12:43 +0300

    header content added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { IoSearchOutline } from "react-icons/io5";
 
 function Header() {
   return (
-    <div>
-      <div>
+    <div className='fixed z-10'>
+      <div className='h-[102px] w-screen bg-opacity-50 border-b border-slate-50 flex flex-row'>
+        <div className='px-[15px] w-[160px]'>
+          <img src="https://exomac.jamstacktemplates.dev/images/logo/logo.png" alt="" className='py-[15px]'/>
+        </div>
+        <div className='text-slate-50'>
+          <button className='text-[16px]'>Homepage</button>
+          <button>About Us</button>
+          <button>Services</button>
+          <button>Work</button>
+          <button>Blog</button>
+          <button>Contact Us</button>
+          <button><IoSearchOutline /></button>
+        </div>
         <div>
-          <h1>EXOMAC</h1>
+          <button className='text-blue-500 '>
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
