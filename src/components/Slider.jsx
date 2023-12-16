c3e8571d75028d454b1e734cd73d5ee33c6e3f0d
gokce <gokcetascii@gmail.com>
2023-12-17 00:12:15 +0300

    Style added for slider gradient color

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,42 +3,41 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import SlickSlider from 'react-slick';
 
-
 function Slider() {
-    const sliders = [
-        {
-            img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-2.jpg"
-        },
-        {
-            img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-1.jpg"
-        },
-        {
-            img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-3.jpg"
-        }
-    ];
+  const sliders = [
+    {
+      img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-2.jpg"
+    },
+    {
+      img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-1.jpg"
+    },
+    {
+      img: "https://exomac.jamstacktemplates.dev/images/hero-image/hero-3.jpg"
+    }
+  ];
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    };
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
 
-    return (
-        <div>
-            <h2>Single Item</h2>
-            <SlickSlider {...settings}>
-                {sliders.map((slider, index) => (
-                    <div key={index}>
-                        <img src={slider.img} alt={`slide ${index + 1}`} />
-                    </div>
-                ))}
-            </SlickSlider>
-        </div>
-    );
+  return (
+    <div className="slider-container relative">
+      <SlickSlider {...settings} >
+        {sliders.map((slider, index) => (
+          <div key={index} className="slider-item relative">
+            <img src={slider.img} alt={`slide ${index + 1}`} />
+            <div className="image-overlay absolute top-0 left-0 w-full h-full bg-gradient-to-r from-[rgba(3, 15, 39, 0.7)] to-[rgba(3, 15, 39, 0)] pointer-events-none"></div>
+          </div>
+        ))}
+      </SlickSlider>
+    </div>
+  );
 }
 
 export default Slider;
