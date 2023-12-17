c0c6f4ceed74c7b2eb5e8ff73e84c61331e9b2c2
gokce <gokcetascii@gmail.com>
2023-12-17 15:24:13 +0300

    Footer page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div>
         <Header/>
         <Home/>
+        <Footer/>
       </div>
     </Router>
   );
