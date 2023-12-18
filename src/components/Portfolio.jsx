d376ba9282a16bb0ad2d5d77c4ff2a529b743a07
gokce <gokcetascii@gmail.com>
2023-12-18 16:31:23 +0300

    portfolio page created and style added

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+
+function Portfolio() {
+    const [selectedTitle, setSelectedTitle] = useState('all');
+
+    const titlecontent = {
+        all: (
+            <>
+                <img src="https://exomac.jamstacktemplates.dev/images/portfolio/portfolio-1.jpg" alt="portfolio-1" />
+                <img src="https://exomac.jamstacktemplates.dev/images/portfolio/portfolio-2.jpg" alt="portfolio-2" />
+                <img src="https://exomac.jamstacktemplates.dev/images/portfolio/portfolio-3.jpg" alt="portfolio-3" />
+                <img src="https://exomac.jamstacktemplates.dev/images/portfolio/portfolio-4.jpg" alt="portfolio-4" />
+                <img src="https://exomac.jamstacktemplates.dev/images/portfolio/portfolio-5.jpg" alt="portfolio-5" />
+                <img src="https://exomac.jamstacktemplates.dev/images/portfolio/portfolio-6.jpg" alt="portfolio-6" />
+            </>
+        ),
+        app: (
+            <>
+            </>
+        ),
+        development: (
+            <>
+            </>
+        ),
+        marketing: (
+            <>
+            </>
+        ),
+        software: (
+            <>
+            </>
+        ),
+        uxdesign: (
+            <>
+            </>
+        )
+    };
+
+    const handleTitleClick = (title) => {
+        setSelectedTitle(title)
+    }
+
+    return (
+        <div>
+            <div className='px-[15px] mx-auto container'>
+                <div className='flex flex-row justify-between' >
+                    <div className='mb-[30px] w-5/12'>
+                        <div className='flex flex-row mb-[25px]'>
+                            <div className='w-[70px] mt-6 h-[2px] bg-[#1292ee]'></div>
+                            <p className='text-[18px] text-paragraphlight pl-[15px]'>Portfolios</p>
+                        </div>
+                        <p className='text-[32px] font-medium'>Selected Works.</p>
+                    </div>
+                    <div>
+                        <ul className='flex flex-row px-[15px] mb-[65px] text-paragraphlight'>
+                            <li >
+                                <button onClick={() => handleTitleClick('all')} className={`text-[16px] font-normal py-[5px] px-[15px] hover:text-primary transition duration-300 ease-in-out   ${selectedTitle === 'all' ? 'text-primary' : ''
+                                    }`}>
+                                    All
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={() => handleTitleClick('app')} className={`text-[16px] font-normal py-[5px] px-[15px] hover:text-primary transition duration-300 ease-in-out  ${selectedTitle === 'app' ? 'text-primary' : ''}`}>
+                                    App
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={() => handleTitleClick('development')} className={`text-[16px] font-normal py-[5px] px-[15px] hover:text-primary transition duration-300 ease-in-out  ${selectedTitle === 'development' ? 'text-primary' : ''}`}>
+                                    Development
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={() => handleTitleClick('marketing')} className={`text-[16px] font-normal py-[5px] px-[15px] hover:text-primary transition duration-300 ease-in-out  ${selectedTitle === 'marketing' ? 'text-primary' : ''}`}>
+                                    Marketing
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={() => handleTitleClick('software')} className={`text-[16px] font-normal py-[5px] px-[15px] hover:text-primary transition duration-300 ease-in-out  ${selectedTitle === 'software' ? 'text-primary' : ''}`}>
+                                    Software
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={() => handleTitleClick('uxdesign')} className={`text-[16px] font-normal py-[5px] px-[15px] hover:text-primary transition duration-300 ease-in-out  ${selectedTitle === 'uxdesign' ? 'text-primary' : ''}`}>
+                                    UX Design
+                                </button>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio
