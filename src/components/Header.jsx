5ffb936c5d4fa83eb1f38be65f239f5e31c37fb5
gokce <gokcetascii@gmail.com>
2023-12-22 00:52:19 +0300

    responsive added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { IoSearchOutline } from "react-icons/io5";
 import { IoIosArrowDown } from "react-icons/io";
 import { Link } from 'react-router-dom';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { MdClose } from "react-icons/md";
 
 function Header() {
   const [isWorkDropdownOpen, setIsWorkDropdownOpen] = useState(false);
   const [isBlogDropdownOpen, setIsBlogDropdownOpen] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false); 
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const scrollThreshold = 770; //height değeri belirterek header ın scroll edilince arka plan rengini değiştirmesini kontrol ettim
 
@@ -39,21 +41,31 @@ function Header() {
     };
   }, []);
 
-  return (
-    <div className={`fixed z-10 bg-opacity-50 border-b border-gray-400 flex flex-row items-center ${isScrolled ? 'bg-changed-color' : ''}`}>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);  //ekran kuculunce hamburger menu acilsin!
 
-      <div className='w-screen bg-opacity-50 border-b border-gray-400 flex flex-row items-center ' >
-        <div className='container mx-auto px-[15px] flex flex-row justify-between items-center  '>
-          <div className='px-[15px] w-[160px] '>
-            <img src="https://exomac.jamstacktemplates.dev/images/logo/logo.png" alt="" className='py-[15px]' />
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  }
+
+  return (
+    <div className={`fixed z-10  w-full bg-opacity-50 flex flex-row items-center justify-center ${isScrolled ? 'bg-changed-color' : ''}`}>
+
+      <div className='flex w-full flex-row items-center justify-center' >
+        <div className='container mx-auto flex flex-row  justify-between items-center max-w-7xl px-0 lg:px-[15px] py-[15px] lg:py-0 '>
+          <div className='px-[15px] w-[160px'>
+            <img src="https://exomac.jamstacktemplates.dev/images/logo/logo.png" alt="" className='py-[15px] ' />
           </div>
-          <div>
-            <div className='w-auto flex justify-center items-center px-[15px]'>
-              <div className='flex flex-row justify-center items-center'>
+          <div className=''>
+            <div className='w-auto flex justify-center items-center px-[15px] '>
+              <div className='flex flex-row justify-center items-center hidden lg:flex'>
                 <Link to={'/'} className='text-[16px] text-white hover:text-primary transition duration-300 ease-in-out py-10 px-[15px]'>
                   Homepage
                 </Link>
-                <Link to={'/'} className='text-[16px] text-white hover:text-primary transition duration-300 ease-in-out py-10 px-[15px]'>
+                <Link to={'/'} className='text-[16px] text-white hover:text-primary transition duration-300 ease-in-out py-10 px-[15px] '>
                   About Us
                 </Link>
                 <Link to={'/'} className='text-[16px] text-white hover:text-primary transition duration-300 ease-in-out py-10 px-[15px]'>
@@ -98,18 +110,62 @@ function Header() {
                   Contact Us
                 </Link>
               </div>
-              <div className='ml-[35px] flex justify-center items-center px-[15px]'>
+              <div className=' px-[45px] justify-items-end hidden sm:flex lg:hidden'>
+                <button className='text-primary hover:text-white hover:scale-105 hover:bg-primary transition duration-300 ease-in-out text-[14px] font-medium h-[40px] flex justify-center items-center px-5 bg-white rounded-md border border-primary'>
+                  Buy Now
+                </button>
+              </div>
+              <div className='ml-0 xl:ml-[35px] flex justify-center items-center px-0 lg:px-[15px]'>
                 <Link to={'/'} className='text-[26px] text-white px-[5px] py-[6px] hover:text-primary transition duration-300 ease-in-out'>
                   <IoSearchOutline />
                 </Link>
+                <div className='lg:hidden cursor-pointer pl-3'>
+                  <div className={`${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}
+                  >
+                    <RxHamburgerMenu className='w-[30px] h-[30px] text-white hover:text-primary' />
+                  </div>
+                  {isMenuOpen && (
+                    <div className='fixed top-0 right-0 w-full h-full flex place-content-end bg-slate-400 bg-opacity-75 z-30 w-[300px]'>
+                      <div className='w-[300px]'>
+                        <div className='px-[15px] flex flex-row justify-between items-center w-[300px] bg-changed-color py-[10px]' onClick={closeMenu} >
+                          <img src="https://exomac.jamstacktemplates.dev/images/logo/logo.png" alt="" className='py-[15px] w-[120px] ' />
+                          <MdClose className='text-white text-4xl hover:text-primary ' />
+                        </div>
+                        <div>
+                          <ul className='text-[16px] pt-[20px] px-[30px] pb-[100px]'>
+                            <li className='border-b border-white hover:text-[#030e22]  border-opacity-50 text-white py-[20px] transition duration-300 ease-in-out'>
+                              <Link><a href="">Homepage</a></Link>
+                            </li>
+                            <li className='border-b border-white border-opacity-50 hover:text-[#030e22] text-white py-[20px] transition duration-300 ease-in-out'>
+                              <Link><a href="">About Us</a></Link>
+                            </li>
+                            <li className='border-b border-white border-opacity-50 hover:text-[#030e22] text-white py-[20px] transition duration-300 ease-in-out'>
+                              <Link><a href="">Services</a></Link>
+                            </li>
+                            <li className='border-b border-white border-opacity-50 hover:text-[#030e22] text-white py-[20px] transition duration-300 ease-in-out'>
+                              <Link><a href="">Work</a></Link>
+                            </li>
+                            <li className='border-b border-white border-opacity-50 hover:text-[#030e22] text-white py-[20px] transition duration-300 ease-in-out'>
+                              <Link><a href="">Blog</a></Link>
+                            </li>
+                            <li className=' text-white py-[20px] hover:text-[#030e22] transition duration-300 ease-in-out'>
+                              <Link><a href="">Contact Us</a></Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-          <div className=' px-[15px] justify-items-end'>
+          <div className=' px-[15px] justify-items-end hidden lg:flex'>
             <button className='text-primary hover:text-white hover:scale-105 hover:bg-primary transition duration-300 ease-in-out text-[16px] font-medium h-[52px] flex justify-center items-center px-10 bg-white rounded-md border border-primary '>
               Buy Now
             </button>
           </div>
+
         </div>
       </div>
     </div>
