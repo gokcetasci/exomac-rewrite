856dead57b75d3b53eebd627ac4492d20d4d8d26
gokce <gokcetascii@gmail.com>
2023-12-22 03:40:08 +0300

    button scrolltotop added

--- src/App.js
+++ src/App.js
@@ -6,20 +6,38 @@ import Footer from './components/Footer';
 import Portfolio from './components/Portfolio';
 import HomeBlogContent from './components/HomeBlogContent';
 import Subscribes from './components/Subscribes';
+import { IoIosArrowUp } from "react-icons/io";
 
 function App() {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <Router>
       <div>
-        <Header/>
-        <Home/>
-        <Portfolio/>
-        <HomeBlogContent/>
-        <Subscribes/>
-        <Footer/>
+        <Header />
+        <Home />
+        <Portfolio />
+        <HomeBlogContent />
+        <Subscribes />
+        <Footer />
+        <div className={`fixed bottom-24 right-7 z-40`}>
+          <button className="bg-primary bg-opacity-50 text-white rounded-full flex justify-center items-center w-12 h-12 hover:scale-110 transition duration-300 ease-in-out"
+            style={{ position: 'fixed', bottom: '2rem', right: '1rem' }}
+            onClick={scrollToTop}
+          >
+            <IoIosArrowUp />
+          </button>
+        </div>
       </div>
     </Router>
   );
 }
+
 
 export default App;
